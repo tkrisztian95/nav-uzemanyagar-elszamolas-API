eddb08264d87f40872e4d2d4f50d4bd9f4a4aa37
Tóth Krisztián Gyula <t.krisztian95@gmail.com>
2020-12-24 12:34:35 +0100

    Only in memory cache by default

--- src/app/default_config.py
+++ src/app/default_config.py
@@ -1,6 +1,6 @@
 class Config(object):
     JSON_AS_ASCII = False
     NO_CACHE = False
-    USE_REDIS_CACHE = True
+    USE_REDIS_CACHE = False
     REDIS_HOST_URL = "redis"
-    USE_IN_MEMORY_CACHE = False
+    USE_IN_MEMORY_CACHE = True
